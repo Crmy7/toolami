696753a4a6bf0538192c3a7c01f06f81b23ece22
khababaa <102924962+khababaa@users.noreply.github.com>
2023-11-23 15:33:27 +0100

    wiki without style

---
+++ src/pages/RealHome.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="home-container">
+        <!-- Logo -->
+        <img src="path/to/your/logo.png" alt="Logo" class="logo">
+
+        <!-- Boutons -->
+        <RouterLink to="/Random"><button  class="btn">Random Tool</button></RouterLink>
+        <RouterLink to="/Home"><button  class="btn">List of Tools</button></RouterLink>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'HomePage',
+
+    
+};
+</script>
+
+<style>
+.home-container {
+    text-align: center;
+    padding: 20px;
+}
+
+.logo {
+    max-width: 200px;
+    margin-bottom: 20px;
+}
+
+.btn {
+    margin: 10px;
+    padding: 10px 20px;
+    font-size: 18px;
+    cursor: pointer;
+    /* Add more styling as per your design */
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
 // Importez le composant Home depuis le bon chemin
+import RealHome from '../pages/RealHome.vue'
+import Home from '../pages/Home.vue'
 import Random from '../pages/Random.vue'
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'RealHome',
+      // Utilisez directement le composant Home ici
+      component: RealHome,
+    },
+    {
+      path: '/Home',
       name: 'Home',
+      // Utilisez directement le composant Home ici
+      component: Home,
+    },
+    {
+      path: '/Random',
+      name: 'Random',
       // Utilisez directement le composant Home ici
       component: Random,
     },
