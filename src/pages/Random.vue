3dd7522b1690b720d4fb69ec5b369c43295303c0
charlesremy <charlesremy03@gmail.com>
2023-11-24 12:56:59 +0100

    fixe random view

--- src/pages/Random.vue
+++ src/pages/Random.vue
@@ -375,16 +375,18 @@ export default {
 }
 
 .card {
-    /* Styles pour la carte */
     border: 1px solid #ddd;
     border-radius: 10px;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+    box-shadow: 0 2px 5px #0003;
     padding: 15px;
     margin-top: 10px;
-    background-color: white;
+    background-color: #fff;
     display: flex;
     flex-wrap: wrap;
     gap: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 }
 
 .card-image {
